ea67cf631469f2dfef1f660bc8c024606cfba29e
Geoffry Millner <geoff.millner22@gmail.com>
2021-07-02 09:58:06 -0700

    added js from project lesson

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+function AddressBook() {
+    this.contacts = {};
+    this.currentId = 0;
+  }
+  
+  AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts[contact.id] = contact;
+  }
+  
+  AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+  }
+  
+  AddressBook.prototype.findContact = function(id) {
+    if (this.contacts[id] != undefined) {
+      return this.contacts[id];
+    }
+    return false;
+  }
+  
+  AddressBook.prototype.deleteContact = function(id) {
+    if (this.contacts[id] === undefined) {
+      return false;
+    }
+    delete this.contacts[id];
+    return true;
+  }
+  
+  function Contact(firstName, lastName, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+  }
+  
+  Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
+
+let addressBook = new AddressBook();
+let contact = new Contact("Jim", "Davis", "555-555-5555");
+let contact2 = new Contact("Garfield", "Cat", "555-555-5555");
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
+addressBook.deleteContact(contact);
+console.log(contact2);
+console.log(contact);
